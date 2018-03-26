97adddef513ccacc287d01b96e8c3e2b53b38499
Aahan Singh <aahansingh95@gmail.com>
2018-03-26 14:50:21 +0800

    Added train acc

--- train.py
+++ train.py
@@ -90,9 +90,8 @@ for epoch in range(10):
         _, pred_label = torch.max(logits.data, dim=1)  # cool trick
         if use_cuda:
             pred_label = pred_label.cuda()
-        total_cnt += x.data.size()[0]
-        correct_cnt += (pred_label == target.data.cuda()).sum()
-        train_acc = correct_cnt / total_cnt
+        correct_cnt = (pred_label == target.data.cuda()).sum()
+        train_acc = correct_cnt/batch_size
         if batch_idx%100==0:
             sys.stdout.write('Epoch = {}\t Batch n.o.={}\t Loss={}\t Loss_m={}\tLoss_r={}\t Train_acc={}\n'
                              .format(epoch,batch_idx,loss.data[0],loss_m.data[0],loss_r.data[0],train_acc))
